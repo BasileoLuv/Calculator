9cde04a6aad32a610fba627dcf4afc4402d97285
BasileoLuv <44769194+BasileoLuv@users.noreply.github.com>
2018-11-16 11:33:29 +0300

    Fixed formalization(PEP8)

--- calc.py
+++ calc.py
@@ -1,19 +1,18 @@
-a = float (raw_input ("Vvedite pervoe chislo = ", ))
-d = str(raw_input ("Vvedite operator (+, -, *, /) = "))
-b = float (raw_input ("Vvedite vtoroe chislo = ", ))
+a = float(raw_input("Vvedite pervoe chislo =",))
+d = str(raw_input("Vvedite operator (+, -, *, /) ="))
+b = float(raw_input("Vvedite vtoroe chislo =",))
+
+if d == '+':
+    e = float(a + b)
+elif d == '-':
+    e = float(a - b)
 
 
-if d == '+' :
- e = float (a + b)
-elif d == '-' :
- e = float (a - b)
+elif d == '*':
+    e = str(a * b)
 
 
-elif d == '*' :
- e = str (a * b)
-
-
-elif d == '/' :
- e = float (a / b)
+elif d == '/':
+    e = float(a / b)
 
 print(e)
